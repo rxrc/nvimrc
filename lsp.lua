ecab62943e8a53717f6d445a6a5e16be5fb8acdc
Evan Sosenko <razorx@evansosenko.com>
2022-08-30 10:57:05 -0500

    Use lsp-zero

--- lsp.lua
+++ lsp.lua
@@ -1,17 +1,5 @@
-local nvim_lsp = require("lspconfig")
+local lsp = require('lsp-zero')
 
-local function setup_servers()
-  require'lspinstall'.setup()
-  local servers = require'lspinstall'.installed_servers()
-  for _, server in pairs(servers) do
-    require'lspconfig'[server].setup{}
-  end
-end
-
-setup_servers()
-
--- Automatically reload after `:LspInstall <server>` so we don't have to restart neovim
-require'lspinstall'.post_install_hook = function ()
-  setup_servers() -- reload installed servers
-  vim.cmd("bufdo e") -- this triggers the FileType autocmd that starts the server
-end
+lsp.preset('recommended')
+lsp.nvim_workspace()
+lsp.setup()
--- plugins.vim
+++ plugins.vim
@@ -173,9 +173,24 @@ let language_groups = {
   \         'javascript', 'javascript.jsx', 'liquid', 'mjml', 'sass', 'scss']
   \ }
 
-"
+" LSP Support
 Plug 'neovim/nvim-lspconfig'
-Plug 'kabouzeid/nvim-lspinstall'
+Plug 'williamboman/mason.nvim'
+Plug 'williamboman/mason-lspconfig.nvim'
+
+" Autocompletion
+Plug 'hrsh7th/nvim-cmp'
+Plug 'hrsh7th/cmp-buffer'
+Plug 'hrsh7th/cmp-path'
+Plug 'saadparwaiz1/cmp_luasnip'
+Plug 'hrsh7th/cmp-nvim-lsp'
+Plug 'hrsh7th/cmp-nvim-lua'
+
+"  Snippets
+Plug 'L3MON4D3/LuaSnip'
+Plug 'rafamadriz/friendly-snippets'
+
+Plug 'VonHeikemen/lsp-zero.nvim'
 
 " Vim-polyglot
 Plug 'sheerun/vim-polyglot'
